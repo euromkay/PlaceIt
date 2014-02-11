76dbadd0fa32c3b054eb10bff18648463dd3c3e8
Charles <euromkay@gmail.com>
2014-02-11 01:29:13 -0800

    Fixed Github Error
    Comments from github were being rolled into the code.  nothing serious
    though.

--- src/ucsd/cse105/placeit/ListActivity.java
+++ src/ucsd/cse105/placeit/ListActivity.java
@@ -36,11 +36,7 @@ public class ListActivity extends Activity implements OnCheckedChangeListener, O
 	}
 	private void setUpList(){
 		list = new ArrayList<Integer>();
-<<<<<<< HEAD
 		ArrayList<PlaceIt> list = Database.getAllPlaceIts(this);
-=======
-		ArrayList<PlaceIt> list = Database.getAllPlaceIts();
->>>>>>> 6ae3470971b5119c1f009090a5b077692306aa94
 		for(int i = 0; i < list.size(); i++)
 			addPlaceItToList(list.get(i), i);
 	}
@@ -103,14 +99,9 @@ public class ListActivity extends Activity implements OnCheckedChangeListener, O
 		//gets the id from the checkbox
 		CheckBox cb = (CheckBox) findViewById((3 * id) + 1);
 		long placeItID = Long.parseLong(cb.getText().toString());
-<<<<<<< HEAD
 		
 		Database.removePlaceIt(placeItID, this);
-=======
 		
-		Database.removePlaceIt(placeItID);
-		
->>>>>>> 6ae3470971b5119c1f009090a5b077692306aa94
 		removeLayoutFromScreen(id);
 	}
 	private void removeLayoutFromScreen(int id){
