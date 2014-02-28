24e875e842e800e304c39a42ce8cf97ec38d72d9
Charles <euromkay@gmail.com>
2014-02-27 20:03:17 -0800

    Finished Login Activity

--- src/ucsd/cse105/placeit/LoginActivity.java
+++ src/ucsd/cse105/placeit/LoginActivity.java
@@ -3,7 +3,12 @@ package ucsd.cse105.placeit;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.Window;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class LoginActivity extends Activity implements OnClickListener{
 
@@ -18,6 +23,8 @@ public class LoginActivity extends Activity implements OnClickListener{
 
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_login);
+		
+		findViewById(R.id.login_button).setOnClickListener(this);
 	}
 	
 	private boolean hasLoginCreds(){
@@ -36,6 +43,28 @@ public class LoginActivity extends Activity implements OnClickListener{
 		
 		finish();
 		
+	}
+
+	@Override
+	public void onClick(View v) {
+		String username = ((TextView) findViewById(R.id.login_username)).getText().toString();
+		EditText pwField = (EditText) findViewById(R.id.login_password);
+		String password = pwField.getText().toString();
+		
+		if(validCreds(username, password)){
+			startMapActivity(username);
+		}
+		else{
+			pwField.setText("");
+			Toast.makeText(this, "Incorrect password/username combination", Toast.LENGTH_LONG).show();
+		}
+			
+			
+	}
+	
+	private boolean validCreds(String username, String password){
+
+		return false;
 	}
 	
 }
